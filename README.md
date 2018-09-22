b226149f81c680553b4eb2fb75eca8f465b26a95
Armenio Maurício Ferreira Neto <dehme.armenio@gmail.com>
2018-09-22 13:27:07 -0300

    clean code + fix readme

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # Bootstrap-Investment Simulator
 
-This project was made to learn how to use bootstrap
+This project was made to learn how to use bootstrap, and improve my investment knowledge.
+
+![alt text](https://i.imgur.com/aPZpYma.png)
+
+![alt text](https://i.imgur.com/4u0GeaS.png)
 
 ## Getting Started
 
 ```
--git clone https://github.com/armenio2/Bootstrap-Simulador-de-Investimento.git
+-git clone https://github.com/armenio2/bootstrap-simulador-de-investimento.git
 -Install Node.js
 -npm install
 ```
@@ -14,8 +18,12 @@ This project was made to learn how to use bootstrap
 
 * [HTML5]
 * [CSS]
+* [JAVA SCRIPT]
+* [BOOTSTRAP]
+* [XMLREQUEST]
 
 ## Contributing
 
 http://getbootstrap.com/docs/4.1/getting-started/introduction/
 
+https://github.com/easynvest/simulador-rendafixa
--- mask.js
+++ mask.js
@@ -1,120 +1,99 @@
 
 //------------------------Mascara quantidade -------------------------
-function maskMoney(input){
-            
+function maskMoney(input) {
     var regex = /[^0-9]/g;
-    input.value =  input.value.replace(regex, "");
-    
-    if (input.value>999 && input.value< 10000){
+    input.value = input.value.replace(regex, "");
+    if (input.value > 999 && input.value < 10000) {
         x = input.value;
-        input.value = "R$"+ x.substring(0,1)+"."+ x.substring(1,4);
-    }else if(input.value>9999 && input.value< 100000){
+        input.value = "R$" + x.substring(0, 1) + "." + x.substring(1, 4);
+    } else if (input.value > 9999 && input.value < 100000) {
         x = input.value;
-        input.value = "R$"+ x.substring(0,2)+"."+ x.substring(2,5);
-    }else if(input.value>99999 && input.value< 1000000){
+        input.value = "R$" + x.substring(0, 2) + "." + x.substring(2, 5);
+    } else if (input.value > 99999 && input.value < 1000000) {
         x = input.value;
-        input.value = "R$"+ x.substring(0,3)+"."+ x.substring(3,6);
-    }else if (input.value>999999 && input.value< 10000000){
+        input.value = "R$" + x.substring(0, 3) + "." + x.substring(3, 6);
+    } else if (input.value > 999999 && input.value < 10000000) {
         x = input.value;
-        input.value = "R$"+ x.substring(0,1)+"."+ x.substring(1,4)+"."+x.substring(4,7);
-    }else if(input.value<1000 && input.value>0){
-        input.value =  "R$"+input.value;
-    }else if(input.value <1){
-        input.value ="";
+        input.value = "R$" + x.substring(0, 1) + "." + x.substring(1, 4) + "." + x.substring(4, 7);
+    } else if (input.value < 1000 && input.value > 0) {
+        input.value = "R$" + input.value;
+    } else if (input.value < 1) {
+        input.value = "";
     }
     enableSubmit()
 }
-//------------------------Mascara Fim Quantidade -------------------------
-
 //------------------------Mascara Data -------------------------
-function maskData(input){
-    if(input.value.length ==2){
+function maskData(input) {
+    if (input.value.length == 2) {
         input.value = input.value + "/"
         return true
-    }if(input.value.length ==5){
+    } if (input.value.length == 5) {
         input.value = input.value + "/"
         return true
-    }if(input.value.length ==10){
+    } if (input.value.length == 10) {
         var value = input.value.split("/")
         var day = value[0]
         var month = value[1]
         var year = value[2]
-            //validando data
-        if(month <13){
-            if(day<32){
-                if(year>2017 && year<2051){
+        //validando data
+        if (month < 13) {
+            if (day < 32) {
+                if (year > 2017 && year < 2051) {
                     //validando fevereiro e outros meses
-                    if(month == 4 || month == 6 || month == 9 || month == 11){
+                    if (month == 4 || month == 6 || month == 9 || month == 11) {
 
-                        if(day <31){
+                        if (day < 31) {
                             enableSubmit()
 
-                        }else{
+                        } else {
                             input.value = "";
                             alert("Favor Informar uma Data Valida")
                         }
-                    }else if(month == 2){
-                        if(day <29){
+                    } else if (month == 2) {
+                        if (day < 29) {
                             enableSubmit()
-                        }else if(day == 29){ //ano bixesto
-                            var rest = year%4
-                            if(rest==0){
+                        } else if (day == 29) { //ano bixesto
+                            var rest = year % 4
+                            if (rest == 0) {
                                 enableSubmit()
-                            }else{
+                            } else {
                                 input.value = "";
                                 alert("Favor Informar uma Data Valida")
                             }
-                            
                         }
-                    }else{
+                    } else {
                         enableSubmit()
                     }
-                }else{
+                } else {
                     input.value = "";
                     alert("Favor Informar uma Data Valida")
                 }
-            }else{
+            } else {
                 input.value = "";
                 alert("Favor Informar uma Data Valida")
             }
-
-        }else{
+        } else {
             input.value = "";
             alert("Favor Informar uma Data Valida")
         }
     }
     var input = document.getElementById('data'); //backspace press
-
-    input.onkeydown = function() {
-    var key = event.keyCode || event.charCode;
-
-    if( key == 8 || key == 46 )
-        input.value = "";
-
+    input.onkeydown = function () {
+        var key = event.keyCode || event.charCode;
+        if (key == 8 || key == 46)
+            input.value = "";
     };
 }
-
-//------------------------Mascara Fim Data -------------------------
-
 //------------------------Mascara Cdi -------------------------
-function maskCdi(input){
-            
+function maskCdi(input) {
     var regex = /[^0-9]/g;
-    input.value =  input.value.replace(regex, "");
-    
-    input.value =  input.value+"%";
+    input.value = input.value.replace(regex, "");
+    input.value = input.value + "%";
     enableSubmit()
-
     var input = document.getElementById('cdi'); //backspace press
-
-    input.onkeydown = function() {
-    var key = event.keyCode || event.charCode;
-
-    if( key == 8 || key == 46 )
-        input.value = "";
-
+    input.onkeydown = function () {
+        var key = event.keyCode || event.charCode;
+        if (key == 8 || key == 46)
+            input.value = "";
     };
 }
-
-
-//------------------------Mascara Fim Cdi -------------------------
